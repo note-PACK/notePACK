52f99730ab3d2e0b129130cda77de09a7a65974d
callummiles <cwmiles18@gmail.com>
2024-03-05 18:40:57 -0600

    installed bcrypt and implemented hashing for user passwords

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const pool = require('../db');
+const bcrypt = require('bcrypt');
 
 const userController = {};
 
@@ -34,14 +35,17 @@ userController.createUser = async (req, res, next) => {
   }
 
   try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
     const newUserQuery = `
     INSERT INTO users (username, password) 
     VALUES ($1, $2) 
     RETURNING id, username;`;
 
-    const newUser = await pool.query(newUserQuery, [username, password]);
+    const newUser = await pool.query(newUserQuery, [username, hashedPassword]);
 
-    res.locals.newUser = newUser;
+    res.locals.newUser = newUser.rows[0];
 
     return next();
   } catch (err) {
@@ -50,6 +54,55 @@ userController.createUser = async (req, res, next) => {
       status: 500,
       message: {
         err: 'Error occured in userController.createUser. Check server logs.',
+      },
+    });
+  }
+};
+
+userController.verifyUser = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return next({
+      log: 'Missing username or password in userController.verifyUser',
+      status: 400,
+      message: { err: 'Missing username or password.' },
+    });
+  }
+
+  try {
+    const userQuery = 'SELECT * FROM users WHERE username = $1';
+    const queryResult = await pool.query(userQuery, [username]);
+
+    if (queryResult.rows.length === 0) {
+      return next({
+        log: 'Incorrect username or password in userController.verifyUser.',
+        status: 400,
+        message: { err: 'Incorrect username or password.' },
+      });
+    }
+
+    const user = queryResult.rows[0];
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return next({
+        log: 'Incorrect username or password in userController.verifyUser.',
+        status: 400,
+        message: { err: 'Incorrect username of password.' },
+      });
+    }
+
+    res.locals.user = { id: user.id, username: user.username };
+
+    return next();
+  } catch (err) {
+    return next({
+      log: `userController.verifyUser: ERROR ${err}`,
+      status: 500,
+      message: {
+        err: 'Error occured in userController.verifyUser. Check server logs.',
       },
     });
   }
